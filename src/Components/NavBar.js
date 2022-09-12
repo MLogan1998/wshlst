c5837c3d06e23e7388c081a08febd2f1c3a7ccf8
Matt Logan <mlogan5212@gmail.com>
2022-09-11 23:48:45 -0500

    update nav bar and deposit component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -22,6 +22,9 @@ export const NavBar = () => {
       <nav className="navigation__nav">
           <ul className="navigation__list">
               <li className="navigation__item"><a href="/home" className="navigation__link" onClick={onClickEvent}>Home</a></li>
+              <li className="navigation__item"><a href="/list" className="navigation__link" onClick={onClickEvent}>Wishlist</a></li>
+              <li className="navigation__item"><a href="/deposit" className="navigation__link" onClick={onClickEvent}>Deposit</a></li>
+              <li className="navigation__item"><a href="/transactions" className="navigation__link" onClick={onClickEvent}>Transactions</a></li>
               <li className="navigation__item"><Link to="/register" className="navigation__link" onClick={handleLogOut}>Log Out</Link></li>
           </ul>
       </nav>
